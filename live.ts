f0889ce52eead2a8523f5fc373bfa8f92ca8274d
Igor Brasileiro <brasileiro456@gmail.com>
2022-09-14 15:51:14 -0300

    chore: Bump live and use fresh/twind plugin (#4)

--- live.ts
+++ live.ts
@@ -1,7 +1,14 @@
 import manifest from "./deco.gen.ts";
 import { start } from "$live/server.ts";
+import stylesPlugin from "$live/plugins/styles.ts";
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.ts";
 
 await start(manifest, {
   site: "start",
   domains: ["start.deco.cx", "golive.deno.dev"],
+  plugins: [
+    stylesPlugin(),
+    twindPlugin(twindConfig),
+  ],
 });
