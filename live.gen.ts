c549739a7ba97c7605d6b65a62d7a926f46df516
Marcos Candeia <marrcooos@gmail.com>
2023-04-19 09:29:55 -0300

    Use live rc60
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- live.gen.ts
+++ live.gen.ts
@@ -16,19 +16,19 @@ import * as $live_inspect from "$live/routes/live/inspect.ts";
 import * as $live_meta from "$live/routes/live/_meta.ts";
 import * as $live_previews from "$live/routes/live/previews/[...block].tsx";
 import * as $live_catchall from "$live/routes/[...catchall].tsx";
-import * as i1$0 from "$live/handlers/routesSelection.ts";
-import * as i1$1 from "$live/handlers/router.ts";
-import * as i1$2 from "$live/handlers/devPage.ts";
-import * as i1$3 from "$live/handlers/fresh.ts";
+import * as i1$0 from "$live/handlers/devPage.ts";
+import * as i1$1 from "$live/handlers/fresh.ts";
+import * as i1$2 from "$live/handlers/router.ts";
+import * as i1$3 from "$live/handlers/routesSelection.ts";
 import * as i1$$0 from "$live/pages/LivePage.tsx";
 import * as i1$$$0 from "$live/sections/PageInclude.tsx";
-import * as i1$$$$0 from "$live/matchers/MatchDate.ts";
-import * as i1$$$$1 from "$live/matchers/MatchUserAgent.ts";
-import * as i1$$$$2 from "$live/matchers/MatchSite.ts";
+import * as i1$$$$0 from "$live/matchers/MatchAlways.ts";
+import * as i1$$$$1 from "$live/matchers/MatchDate.ts";
+import * as i1$$$$2 from "$live/matchers/MatchEnvironment.ts";
 import * as i1$$$$3 from "$live/matchers/MatchMulti.ts";
 import * as i1$$$$4 from "$live/matchers/MatchRandom.ts";
-import * as i1$$$$5 from "$live/matchers/MatchEnvironment.ts";
-import * as i1$$$$6 from "$live/matchers/MatchAlways.ts";
+import * as i1$$$$5 from "$live/matchers/MatchSite.ts";
+import * as i1$$$$6 from "$live/matchers/MatchUserAgent.ts";
 import * as i1$$$$$0 from "$live/flags/audience.ts";
 import * as i1$$$$$1 from "$live/flags/everyone.ts";
 
@@ -39,36 +39,37 @@ const manifest = {
   "routes": {
     "./routes/_app.tsx": $$$$0,
     "./routes/_middleware.ts": $live_middleware,
-    "./routes/live/workbench.ts": $live_workbench,
-    "./routes/live/invoke/index.ts": $live_invoke,
+    "./routes/[...catchall].tsx": $live_catchall,
+    "./routes/index.tsx": $live_catchall,
+    "./routes/live/_meta.ts": $live_meta,
     "./routes/live/editorData.ts": $live_editorData,
     "./routes/live/inspect.ts": $live_inspect,
-    "./routes/live/_meta.ts": $live_meta,
+    "./routes/live/invoke/index.ts": $live_invoke,
     "./routes/live/previews/[...block].tsx": $live_previews,
-    "./routes/[...catchall].tsx": $live_catchall,
+    "./routes/live/workbench.ts": $live_workbench,
   },
   "sections": {
-    "deco-sites/start/sections/Head.tsx": $$$$$$$$0,
-    "deco-sites/start/sections/GetStarted.tsx": $$$$$$$$1,
     "$live/sections/PageInclude.tsx": i1$$$0,
+    "deco-sites/start/sections/GetStarted.tsx": $$$$$$$$1,
+    "deco-sites/start/sections/Head.tsx": $$$$$$$$0,
   },
   "handlers": {
-    "$live/handlers/routesSelection.ts": i1$0,
-    "$live/handlers/router.ts": i1$1,
-    "$live/handlers/devPage.ts": i1$2,
-    "$live/handlers/fresh.ts": i1$3,
+    "$live/handlers/devPage.ts": i1$0,
+    "$live/handlers/fresh.ts": i1$1,
+    "$live/handlers/router.ts": i1$2,
+    "$live/handlers/routesSelection.ts": i1$3,
   },
   "pages": {
     "$live/pages/LivePage.tsx": i1$$0,
   },
   "matchers": {
-    "$live/matchers/MatchDate.ts": i1$$$$0,
-    "$live/matchers/MatchUserAgent.ts": i1$$$$1,
-    "$live/matchers/MatchSite.ts": i1$$$$2,
+    "$live/matchers/MatchAlways.ts": i1$$$$0,
+    "$live/matchers/MatchDate.ts": i1$$$$1,
+    "$live/matchers/MatchEnvironment.ts": i1$$$$2,
     "$live/matchers/MatchMulti.ts": i1$$$$3,
     "$live/matchers/MatchRandom.ts": i1$$$$4,
-    "$live/matchers/MatchEnvironment.ts": i1$$$$5,
-    "$live/matchers/MatchAlways.ts": i1$$$$6,
+    "$live/matchers/MatchSite.ts": i1$$$$5,
+    "$live/matchers/MatchUserAgent.ts": i1$$$$6,
   },
   "flags": {
     "$live/flags/audience.ts": i1$$$$$0,
