360931b5e4ea0310b3f655e6583ba239422a9f1d
Augusto Barbosa <augustocbarbosa@gmail.com>
2023-07-17 18:33:41 -0300

    Update files

--- live.gen.ts
+++ live.gen.ts
@@ -5,11 +5,10 @@
 import { DecoManifest } from "$live/types.ts";
 import * as $$$$0 from "./routes/styles.css.ts";
 import * as $$$$1 from "./routes/_app.tsx";
-import * as $$$$$$$$0 from "./sections/Projects.tsx";
-import * as $$$$$$$$1 from "./sections/Hero.tsx";
-import * as $$$$$$$$2 from "./sections/Theme.tsx";
-import * as $$$$$$$$3 from "./sections/SimpleTextList.tsx";
-import * as $$$$$$$$4 from "./sections/ParagraphList.tsx";
+import * as $$$$$$$$0 from "./sections/Hero.tsx";
+import * as $$$$$$$$1 from "./sections/Theme.tsx";
+import * as $$$$$$$$2 from "./sections/SimpleTextList.tsx";
+import * as $$$$$$$$3 from "./sections/ParagraphList.tsx";
 import * as $live_workflows from "$live/routes/live/workflows/run.ts";
 import * as $live_middleware from "$live/routes/_middleware.ts";
 import * as $live_workbench from "$live/routes/live/workbench.ts";
@@ -159,11 +158,10 @@ const manifest = {
     "$live/sections/PageInclude.tsx": i2$$$$$2,
     "$live/sections/Slot.tsx": i2$$$$$3,
     "$live/sections/UseSlot.tsx": i2$$$$$4,
-    "deco-sites/start/sections/Hero.tsx": $$$$$$$$1,
-    "deco-sites/start/sections/ParagraphList.tsx": $$$$$$$$4,
-    "deco-sites/start/sections/Projects.tsx": $$$$$$$$0,
-    "deco-sites/start/sections/SimpleTextList.tsx": $$$$$$$$3,
-    "deco-sites/start/sections/Theme.tsx": $$$$$$$$2,
+    "deco-sites/start/sections/Hero.tsx": $$$$$$$$0,
+    "deco-sites/start/sections/ParagraphList.tsx": $$$$$$$$3,
+    "deco-sites/start/sections/SimpleTextList.tsx": $$$$$$$$2,
+    "deco-sites/start/sections/Theme.tsx": $$$$$$$$1,
     "deco-sites/std/sections/Analytics.tsx": i2$$$$$5,
     "deco-sites/std/sections/configButterCMS.global.tsx": i2$$$$$6,
     "deco-sites/std/sections/configNuvemShop.tsx": i2$$$$$7,
