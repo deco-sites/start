59c0e42ab8aed636759a9eb878cd46dbf6706e93
gimenes <tlgimenes@gmail.com>
2023-12-07 11:21:38 -0300

    regen fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,14 +2,16 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/_app.tsx";
+import * as $_app from "./routes/_app.tsx";
+
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/_app.tsx": $0,
+    "./routes/_app.tsx": $_app,
   },
   islands: {},
   baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;
