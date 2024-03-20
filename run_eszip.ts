d843103cd99609170d2316d90f59d6e525d018c6
devartes <anacavalcante731@gmail.com>
2024-03-20 17:56:24 -0300

    chore: remove files

--- run_eszip.ts
+++
@@ -1,7 +0,0 @@
-import { runEsZip } from "./eszip_lib.ts";
-
-await runEsZip(
-  "out.eszip2",
-  "https://denopkg.com/deco-sites/start@main/main.ts",
-  "https://denopkg.com/deco-sites/start@main/deno.json",
-);
