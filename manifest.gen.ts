209d0370407177a3397fdd66da9142c331335345
Augusto Barbosa <augustocbarbosa@gmail.com>
2024-03-25 17:00:42 -0300

    Remove duplicated section

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -5,13 +5,11 @@
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/ItWorks.tsx";
-import * as $$$$$$2 from "./sections/sections/ItWorks.tsx";
 import * as $$$$$$1 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
     "deco-sites/start/sections/ItWorks.tsx": $$$$$$0,
-    "deco-sites/start/sections/sections/ItWorks.tsx": $$$$$$2,
     "deco-sites/start/sections/Theme/Theme.tsx": $$$$$$1,
   },
   "apps": {
