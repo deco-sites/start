675c8fad69cf6a4f334f4e97f5e72f747c42f21b
devartes <anacavalcante731@gmail.com>
2024-03-25 14:02:28 -0300

    refactor: default name change

--- sections/ItWorks.tsx
+++ sections/ItWorks.tsx
@@ -1,15 +1,14 @@
 export interface Props {
-    /**
-     * @description The description of name.
-     */
-    name?: string;
-  }
-  
-  export default function Section({ name = "Capy" }: Props) {
-    return (
-      <div class="flex h-96 items-center justify-center mt-10">
-        <p class="leading-10 text-6xl">{name}</p>
-      </div>
-    );
-  }
-  
+  /**
+   * @description The description of name.
+   */
+  name?: string;
+}
+
+export default function Section({ name = "It Works!" }: Props) {
+  return (
+    <div class="flex h-96 items-center justify-center">
+      <p class="leading-10 text-6xl">{name}</p>
+    </div>
+  );
+}
