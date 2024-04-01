8cf0afc9e6766db7680b0637949dac16082d386f
Tiago Gimenes <gimenes@deco.cx>
2024-04-01 03:32:55 +0000

    Release Staging environment with 0 changes made by

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -24,12 +24,6 @@ export default defineApp(async (_req, ctx) => {
 
         {/* Web Manifest */}
         <link rel="manifest" href={asset("/site.webmanifest")} />
-
-        {/* Install HTMX */}
-        <script
-          src="https://cdn.jsdelivr.net/npm/htmx.org@1.9.11"
-          crossorigin="anonymous"
-        />
       </Head>
 
       {/* Rest of Preact tree */}
