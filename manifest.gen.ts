20082c59b68534b0609e5e563532584055d8666d
Marcos Candeia <candeia@deco.cx>
2024-05-24 10:35:25 +0000

    Release staging environment with 3 changes made by Marcos Candeia

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,7 +2,8 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$0 from "./apps/site.ts";
+import * as $$$$$$$$$$$0 from "./apps/deco/htmx.ts";
+import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/Footer.tsx";
 import * as $$$$$$1 from "./sections/ItWorks.tsx";
 import * as $$$$$$2 from "./sections/Theme/Theme.tsx";
@@ -14,7 +15,8 @@ const manifest = {
     "site/sections/Theme/Theme.tsx": $$$$$$2,
   },
   "apps": {
-    "site/apps/site.ts": $$$$$$$$$$$0,
+    "site/apps/deco/htmx.ts": $$$$$$$$$$$0,
+    "site/apps/site.ts": $$$$$$$$$$$1,
   },
   "name": "site",
   "baseUrl": import.meta.url,
