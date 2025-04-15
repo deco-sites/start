e5b688459d61f9a0b636ba6ca7a5cad669e9d9f1
guitavano <tavano62@gmail.com>
2025-04-15 16:33:06 -0300

    bump

--- runtime.ts
+++ runtime.ts
@@ -1,6 +1,3 @@
-import { proxy } from "deco/clients/withManifest.ts";
 import type { Manifest } from "./manifest.gen.ts";
-
-export const invoke = proxy<
-  Manifest
->();
+import { proxy } from "@deco/deco/web";
+export const invoke = proxy<Manifest>();
